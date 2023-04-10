647647b5191844feaafb9ec1ee32b4ab4792e19b
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-04-10 23:54:51 +0300

    added send test email

--- services/emailService.js
+++ services/emailService.js
@@ -1,12 +1,37 @@
 const { getVerificationUrl } = require("../helpers");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const sendVerificationEmail = ({ email, verificationToken }) => {
+const sendVerificationEmail = async ({ email, verificationToken }) => {
   const verificationURL = getVerificationUrl(verificationToken);
 
   //TODO: Send email
   console.log("TODO: Send email");
   console.log("email ", email);
   console.log("verificationURL ", verificationURL);
+  console.log("process.env.MAILTRAP_USER ", process.env.MAILTRAP_USER);
+  console.log("process.env.MAILTRAP_PASSWORD ", process.env.MAILTRAP_PASSWORD);
+
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transport.sendMail({
+    from: '"Fred Foo 👻" <dmytro.muzyka@meta.ua>',
+    to: "joceb34719@djpich.com",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: `<b>Hello world? ${verificationURL}</b>`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   const message = "Verification email sent";
 
